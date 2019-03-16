397f6404604efa2d88248c748b34c028e70953ca
Kim-JunHyeong <jhkim4685@gmail.com>
2019-03-16 23:00:36 +0900

    Visitor Pattern

---
+++ src/com/examples/crazy/visitor/Visitable.java
@@ -0,0 +1,5 @@
+package com.examples.crazy.visitor;
+
+public interface Visitable {
+    public void accept(Visitor visitor);
+}
---
+++ src/com/examples/crazy/visitor/Visitor.java
@@ -0,0 +1,5 @@
+package com.examples.crazy.visitor;
+
+public interface Visitor {
+    public void visit(Visitable visitable);
+}
